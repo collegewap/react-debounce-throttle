99f672356c0913a80de12a21a3f698a65887e138
Abhishek EH <collegewap@gmail.com>
2021-04-27 16:56:51 +0530

    Using debounce

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import { useRef, useState } from "react";
 function App() {
   const inputRef = useRef();
   const [animals, setAnimals] = useState([]);
+  const timeout = useRef();
 
   const handleDebounceSearch = () => {
+    //Clear the previous timeout.
+    clearTimeout(timeout.current);
+
     // If there is no search term, do not make API call
     if (!inputRef.current.value.trim()) {
       setAnimals([]);
       return;
     }
-    fetch(`http://localhost:4000/animals?q=${inputRef.current.value}`)
-      .then(async (response) => {
-        if (!response.ok) {
-          console.log("Something went wrong!");
-        } else {
-          const data = await response.json();
-          setAnimals(data);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    timeout.current = setTimeout(() => {
+      fetch(`http://localhost:4000/animals?q=${inputRef.current.value}`)
+        .then(async (response) => {
+          if (!response.ok) {
+            console.log("Something went wrong!");
+          } else {
+            const data = await response.json();
+            setAnimals(data);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }, 600);
   };
 
   return (
