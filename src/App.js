a49000cb8a9fbfdc3bf71069c784286ced7bc174
Abhishek EH <collegewap@gmail.com>
2021-04-27 17:19:09 +0530

    Adding throttle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [animals, setAnimals] = useState([]);
   const timeout = useRef();
 
+  const throttling = useRef(false);
+
   const handleDebounceSearch = () => {
     //Clear the previous timeout.
     clearTimeout(timeout.current);
@@ -30,12 +32,39 @@ function App() {
     }, 600);
   };
 
+  const handleThrottleSearch = () => {
+    if (throttling.current) {
+      return;
+    }
+    // If there is no search term, do not make API call
+    if (!inputRef.current.value.trim()) {
+      setAnimals([]);
+      return;
+    }
+    throttling.current = true;
+    setTimeout(() => {
+      throttling.current = false;
+      fetch(`http://localhost:4000/animals?q=${inputRef.current.value}`)
+        .then(async (response) => {
+          if (!response.ok) {
+            console.log("Something went wrong!");
+          } else {
+            const data = await response.json();
+            setAnimals(data);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }, 600);
+  };
+
   return (
     <div>
       <input
         type="text"
         ref={inputRef}
-        onChange={handleDebounceSearch}
+        onChange={handleThrottleSearch}
         className="search-input"
       />
       {/* Display the result if search term is not empty and results are present */}
